7797d7481866b31bcaf36ba9c5a67f91196193df
Muthu V <132546801+Muthu4558@users.noreply.github.com>
2025-03-12 11:44:44 +0530

    Update NavbarWeb.jsx

--- client/src/components/NavbarWeb.jsx
+++ client/src/components/NavbarWeb.jsx
@@ -53,7 +53,7 @@ const NavbarWeb = () => {
         setMessage("");
 
         try {
-            const response = await fetch("http://localhost:5000/api/signup", {
+            const response = await fetch("${import.meta.env.VITE_APP_BASE_URL}/api/signup", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
