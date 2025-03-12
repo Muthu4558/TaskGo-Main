b00d11268374e58c1fd7de0e5991ce420603366c
Muthu V <132546801+Muthu4558@users.noreply.github.com>
2025-03-12 12:50:54 +0530

    Update NavbarWeb.jsx

--- client/src/components/NavbarWeb.jsx
+++ client/src/components/NavbarWeb.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import gsap from "gsap";
 import logo from "../assets/images/WebsiteImg/logo2.png";
-import { RiContactsFill } from "react-icons/ri";
+import { RiContactsFill, RiUserAddFill } from "react-icons/ri";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Loading from "./Loader";
 
 const NavbarWeb = () => {
@@ -15,7 +17,6 @@ const NavbarWeb = () => {
         email: "",
         userCount: "",
     });
-    const [message, setMessage] = useState("");
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
@@ -37,11 +38,11 @@ const NavbarWeb = () => {
     const handleSubmit = async () => {
         const { name, company, email, userCount } = formData;
         if (!name || !company || !email || !userCount) {
-            setMessage("All fields are required!");
+            toast.error("All fields are required!");
             return;
         }
+
         setLoading(true);
-        setMessage("");
 
         try {
             const response = await fetch(`${import.meta.env.VITE_APP_BASE_URL}/api/signup`, {
@@ -51,38 +52,40 @@ const NavbarWeb = () => {
             });
 
             if (response.ok) {
-                setMessage("Email sent successfully!");
+                toast.success("Successfully! Signed up.");
                 setTimeout(() => {
                     setIsOpen(false);
-                    setMessage("");
                     setFormData({ name: "", company: "", email: "", userCount: "" });
                 }, 2000);
             } else {
-                setMessage("Failed to send email.");
+                toast.error("Failed to send email.");
             }
         } catch (error) {
-            setMessage("Error sending email.");
+            toast.error("Error sending email.");
         }
+        
         setLoading(false);
     };
 
     return (
         <>
+            <ToastContainer position="bottom-right" autoClose={3000} hideProgressBar={false} />
             <nav className="bg-white shadow-md fixed w-full z-50 top-0">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex justify-between h-24">
                         <div className="flex-shrink-0 flex items-center">
                             <img className="w-40 lg:w-48" src={logo} alt="Company Logo" />
                         </div>
-                        <div className="flex items-center space-x-6">
+                        <div className="flex flex-col md:flex-row items-center md:space-x-6 space-y-3 md:space-y-0">
                             <button
                                 onClick={togglePopup}
-                                className="font-bold text-sm text-[#229ea6] hover:underline transition"
+                                className="flex items-center gap-2 px-4 py-2 bg-[#229ea6] text-white rounded-lg hover:bg-black transition transform hover:scale-105 w-full md:w-auto justify-center mt-1"
                             >
-                                Signup For Start Tasking
+                                <RiUserAddFill size={20} />
+                                Signup
                             </button>
                             <button
-                                className="gap-4 px-4 py-2 bg-[#229ea6] text-white rounded-lg hover:bg-black transition transform hover:scale-105 flex items-center"
+                                className="flex items-center gap-2 px-4 py-2 bg-[#229ea6] text-white rounded-lg hover:bg-black transition transform hover:scale-105 w-full md:w-auto justify-center"
                                 onClick={() => navigate("/log-in")}
                             >
                                 <RiContactsFill size={20} />
@@ -144,8 +147,6 @@ const NavbarWeb = () => {
                             Submit
                         </button>
                         )}
-
-                        {message && <p className="mt-3 text-sm text-gray-600">{message}</p>}
 
                         <button
                             onClick={togglePopup}
