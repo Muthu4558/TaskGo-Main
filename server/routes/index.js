df526c6a6a5d84a60fb6ad49c2692ef40f8239d0
Muthu V <132546801+Muthu4558@users.noreply.github.com>
2025-03-12 11:37:43 +0530

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import userRoutes from "./userRoutes.js";
 import taskRoutes from "./taskRoutes.js";
 import dailyReportRoutes from "./dailyReportRoutes.js"
+import { signupUser } from "../controllers/signupContoller.js";
 
 const router = express.Router();
 
 router.use("/user", userRoutes); //api/user/login
 router.use("/task", taskRoutes);
 router.use("/daily-reports", dailyReportRoutes);
+router.use("/signup", signupUser);
 
 export default router;
