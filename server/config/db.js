3cf40f51549109adea70da181bc84262e0b589ed
Muthu4558 <muthukdm45@gmail.com>
2025-07-31 10:27:17 +0530

    token

--- server/config/db.js
+++ server/config/db.js
@@ -18,15 +18,15 @@ export default connectDB;
 
 export const createJWT = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
+    expiresIn: "7d", // Increased token expiration to 7 days
   });
 
-  // Change sameSite from strict to none when you deploy your app
+  // Set cookie with matching expiration
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "none", //prevent CSRF attack
-    maxAge: 1 * 24 * 60 * 60 * 1000, //1 day
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
 
